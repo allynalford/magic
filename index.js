b36252132813508cccbc7f2a04cbf42abcd7a51f
Allyn Alford <allynalford@gmail.com>
2018-10-09 14:16:53 -0400

    Uploading Images via the REST API

--- index.js
+++ index.js
@@ -11,18 +11,43 @@ const upload = multer({ dest: 'uploads/' })
 
 var fs = require('fs');
 var path = require('path');
+var request = require('request');
 
 app.post('/file_upload', upload.single('file'), (req, res, next) => {
   // req.file is the `example` file or whatever you have on the `name` attribute: <input type="file" name="example" />
   // I believe it is a `Buffer` object.
-  const encoded = req.file.toString('base64')
-  console.log(encoded)
+  const encoded = req.file.toString('base64');
+  
+  var filename = req.file.originalname;
+  var username = req.body.username;
+  
+  
+  request.post(
+    'https://v827vdxy7h.execute-api.us-east-1.amazonaws.com/prod/upload-to-s3',
+    { json: { username: username, filename: filename, user_avatar: encoded} },
+    function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            console.log(body)
+			
+			res.send('File ' + filename +' Uploaded to ML/' + username + '/')
+        }else{
+			res.send(error)
+		}
+    }
+)
+  
+  
+  
 })
 
-//app.get('/', (req, res) => res.send('Hello World!'))
+
 
 app.get('/', function (req, res) {
-   res.sendFile('public${path.sep}index.html');
+   //res.sendFile('./public/index.html');
+   //res.sendFile(path.join(__dirname + '/index.html'));
+   
+   var contents = fs.readFileSync(`public${path.sep}index.html`);
+	res.send(contents.toString());
 })
 
 
