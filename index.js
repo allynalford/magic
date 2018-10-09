c3539402729e13bcd0ed71981733e99c68fe6155
Allyn Alford <allynalford@gmail.com>
2018-10-09 12:55:23 -0400

    express

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 'use strict';
 
-var fs = require('fs');
-var path = require('path');
 
-exports.get = function(event, context, callback) {
-  var contents = fs.readFileSync(`public${path.sep}index.html`);
-  var result = {
-    statusCode: 200,
-    body: contents.toString(),
-    headers: {'content-type': 'text/html'}
-  };
 
-  callback(null, result);
-};
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
