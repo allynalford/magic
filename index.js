353ea0a5f7d6636a1e6200a71bb9ffa1adf7e09e
Allyn Alford <allynalford@gmail.com>
2018-10-09 12:57:14 -0400

    Express with POST

--- index.js
+++ index.js
@@ -4,7 +4,19 @@
 
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 80
+
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
+
+
+
+app.post('/file_upload', upload.single('file'), (req, res, next) => {
+  // req.file is the `example` file or whatever you have on the `name` attribute: <input type="file" name="example" />
+  // I believe it is a `Buffer` object.
+  const encoded = req.file.toString('base64')
+  console.log(encoded)
+})
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
